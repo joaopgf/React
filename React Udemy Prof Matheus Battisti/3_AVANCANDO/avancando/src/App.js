f6f3fd54b8522c5fb2c2e5e0535e935236ae3ac3
João Pedro <joaopgf.it@gmail.com>
2024-09-24 16:50:07 -0300

    imagens na parte 3

--- React Udemy Prof Matheus Battisti/3_AVANCANDO/avancando/src/App.js
+++ React Udemy Prof Matheus Battisti/3_AVANCANDO/avancando/src/App.js
@@ -1,10 +1,21 @@
 
 import './App.css';
+import city from "../public/assets/city.jpg";
+
 
 function App() {
   return (
     <div className="App">
       <h1>Avançando com React</h1>
+      {/*Imagem em public*/}
+      <div>
+        <img src="/img1.jpg" alt="paisagem" />
+      </div>
+
+      {/*Imagem em src/assets*/}
+      <div>
+        <img src={city} alt="cidade" />
+      </div>
     </div>
   );
 }
